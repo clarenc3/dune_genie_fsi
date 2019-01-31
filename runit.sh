c68d86a464e17ae3675a666e499ced19db6318d3
clarenc3 <cwret@fnal.gov>
2019-01-31 15:52:45 +0100

    Add submission script for different modes

--- runit.sh
+++ runit.sh
@@ -2,5 +2,8 @@
 # This runs on NUISANCE flat-trees and gets 3D cross-sections
 
 for i in $(ls *.root_FlatTree.root); do
-  root -l -b -q 'fsianal.cpp("'$i'")' &
+  for j in CCQE 2p2h 1pi MpiDIS; do
+    root -l -b -q 'fsianal.cpp("'$i'", "'$j'")' &
+    #root -l -b -q 'fsianal_skew.cpp("'$i'")' &
+  done
 done
